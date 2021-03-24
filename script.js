da78580e8bc8e03b07d9a5fe4a3b485c754d1505
Omari Jalagania <omari.jalagania@gmail.com>
2021-03-24 12:21:19 +0400

    not-finished

---
+++ script.js
@@ -0,0 +1,47 @@
+var tiny = $('#tiny').tiny().data('api_tiny');
+
+
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        items:4,
+        loop:true,
+        autoplay:true,
+        responsiveClass:true,
+        responsive: {
+            1280:{
+                items:4
+            },
+
+
+            1079:{
+                items:4
+            },
+
+            991: {
+                items: 3
+            },
+
+            757: {
+                items: 3
+            },
+
+            688: {
+                items: 2
+            },
+
+
+            500: {
+                items: 2
+            },
+
+            513: {
+                items: 2
+            },
+
+            200: {
+                items: 1
+            }
+        }
+    });
+  });
